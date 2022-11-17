19b7add171f94ba7343b583748b4d731bd476458
dev4ult <nibrasbiasaaja@gmail.com>
2022-11-17 21:40:18 +0700

    Some font styling done

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,21 +1,53 @@
 import { useState, useEffect } from 'react';
 import FontEditor from './FontEditor.jsx';
+import parse from 'html-react-parser';
 
 function Notes({ handleNoteChange, textNote }) {
-  const [previewMode, setVideMode] = useState(false);
-  const [previewNote, setPreviewNote] = useState('');
+  const [previewMode, setViewMode] = useState(false);
+  const [previewNote, setPreviewNote] = useState('doNotLetEmpty');
 
   useEffect(() => {
-    // changing new line into <br />
-    setPreviewNote(textNote.replace(/\n/g, '<br />'));
+    setPreviewNote(() => {
+      // changing new line into <br />
+      let newNote = textNote.replace(/\n/g, '<br />');
 
-    // heading styling with h2 html tag
-    console.log(previewNote.match(/#/g).length);
-  }, [textNote, previewMode]);
+      function convertSymbolToTag(symbol, tag) {
+        const globalRegEx = new RegExp(symbol, 'g');
+        const regEx = new RegExp(symbol, '');
+
+        let totalSymbol = textNote.match(globalRegEx);
+
+        if (totalSymbol === null) {
+          return false;
+        }
+
+        totalSymbol = totalSymbol.length;
+        totalSymbol = totalSymbol % 2 !== 0 ? totalSymbol - 1 : totalSymbol;
+
+        for (let i = 0; i < totalSymbol; i++) {
+          if (i % 2 === 0) {
+            newNote = newNote.replace(regEx, `<${tag}>`);
+          } else {
+            newNote = newNote.replace(regEx, `</${tag}>`);
+          }
+        }
+      }
+
+      // changing # for heading with <h1> html tag
+      convertSymbolToTag('#', 'h1');
+      convertSymbolToTag('@', 'b');
+      convertSymbolToTag('_', 'i');
+      convertSymbolToTag('~', 's');
+
+      return newNote;
+    });
+
+    setPreviewNote((prevNote) => parse(prevNote));
+  }, [previewMode, textNote]);
 
   function toggleView(e) {
     const { name } = e.target;
-    setVideMode((prevSet) => {
+    setViewMode((prevSet) => {
       return (name === 'btnPreview' && prevSet) || (name === 'btnWrite' && !prevSet) ? prevSet : !prevSet;
     });
   }
