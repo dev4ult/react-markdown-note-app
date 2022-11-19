e5f651b4fdcfbcfc51cc17a97024385fd093a5dc
dev4ult <nibrasbiasaaja@gmail.com>
2022-11-19 11:59:18 +0700

    modal component fixed but need some code refactoring for less or better code

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
   const [inputTextVal, setInputTextVal] = useState('');
 
   const [show, setShow] = useState(false);
-  const [showWarning, setShowWarning] = useState(false);
+  const [modalType, setModalType] = useState('new-note-form');
 
   useEffect(() => {
     if (localStorage.getItem('notes') === null) {
@@ -69,14 +69,6 @@ function App() {
     setInputTextVal(value);
   }
 
-  const [modal, setModal] = useState(
-    <Modal title="Note Title" textInput="Type your note title here" onKeydown={handleKey} onClickAccept={addNote} setShowHandle={setShow} handleInput={handleChange} inputVal={inputTextVal} btnHideModalText="Cancel" />
-  );
-
-  useEffect(() => {
-    setModal(<Modal title="Note Title" textInput="Type your note title here" onKeydown={handleKey} onClickAccept={addNote} setShowHandle={setShow} handleInput={handleChange} inputVal={inputTextVal} btnHideModalText="Cancel" />);
-  }, [inputTextVal]);
-
   function toggleSelect(e) {
     const { id } = e.target;
     const index = notes.map((note) => note.id).indexOf(parseInt(id));
@@ -114,30 +106,44 @@ function App() {
     });
   }
 
-  function fontEditorClicked(symbol) {
-    setModal(<Modal title="Warning!" desc="ayam goreng" setShowHandle={setShow} btnHideModalText="Close" />);
+  useEffect(() => {
+    if (textNote !== '') {
+      const textArea = document.querySelector('textarea');
+      const { value } = textArea;
 
+      setNotes((prevNotes) => {
+        const indexOfSelected = prevNotes.map((note) => note.selected).indexOf(true);
+        const newNotes = prevNotes;
+        newNotes[indexOfSelected].text = value;
+        return [...newNotes];
+      });
+    }
+  }, [textNote]);
+
+  function fontEditorClicked(symbol) {
+    if (!document.querySelector('textarea')) {
+      return;
+    }
     const textArea = document.querySelector('textarea');
     const selectedText = window.getSelection().toString();
     const textLength = selectedText.length;
 
     if (selectedText === '') {
-      setTextNote((prevNote) => `${prevNote} ${symbol} ${symbol} `);
+      setTextNote((prevNote) => `${prevNote} ${symbol}${symbol} `);
       textArea.focus();
     } else {
       const matchingText = textNote.match(new RegExp(selectedText, 'g'));
       if (matchingText.length > 1) {
+        setModalType('warning-sign');
         setShow(true);
-        setTextNote((prevNote) => prevNote + ' ');
       } else {
-        setTextNote((prevNote) => {
-          let newNote = prevNote;
+        setTextNote((prevTextNote) => {
+          let newNote = prevTextNote;
 
           const startIndex = newNote.indexOf(selectedText);
           newNote = newNote.slice(0, startIndex) + `${symbol}${selectedText}${symbol}` + newNote.slice(startIndex + textLength, newNote.length);
 
           textArea.focus();
-
           return newNote;
         });
       }
@@ -147,11 +153,23 @@ function App() {
   return (
     <div className="App">
       <aside>
-        <button className={'heading' + (show ? ' btn-selected' : '')} type="button" onClick={setShow.bind(null, true)}>
+        <button
+          className="heading"
+          type="button"
+          onClick={function () {
+            setModalType('new-note-form');
+            setShow(true);
+          }}
+        >
           <h1 className="title-heading">New Note</h1>
           <div className="btn-show-modal">+</div>
         </button>
-        {show && modal}
+        {show &&
+          (modalType === 'new-note-form' ? (
+            <Modal title="New Note" textInput="Type your new note here" onKeydown={handleKey} onClickAccept={addNote} setShowHandle={setShow} handleInput={handleChange} inputVal={inputTextVal} btnHideModalText="Cancel" />
+          ) : (
+            <Modal title="Warning !" desc="If selected text is matching with more than one string. Please manually type the existing font editor to style your text" setShowHandle={setShow} btnHideModalText="Close" />
+          ))}
         <ul>{noteList}</ul>
       </aside>
       <Notes handleNoteChange={handleNoteChange} textNote={textNote} handleFont={fontEditorClicked} />
