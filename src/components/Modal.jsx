310ed1b7663d65eb06ed476cf4976d00300abd10
dev4ult <nibrasbiasaaja@gmail.com>
2022-11-23 14:53:32 +0700

    minor update

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,9 +1,11 @@
+import parse from 'html-react-parser';
+
 function Modal({ title, desc, textInput, onKeydown, onClickAccept, btnAcceptModalText, setShowHandle, handleInput, inputVal, btnHideModalText }) {
   return (
     <div className="modal-container">
       <div className="modal">
         <h1 className="modal-title">{title}</h1>
-        {true && (desc ? <p className="modal-desc">{desc}</p> : false)}
+        {true && (desc ? <div className="modal-desc">{desc}</div> : false)}
         {true && (textInput ? <input className="modal-input-text" type="text" placeholder={textInput} value={inputVal} onChange={handleInput} onKeyDown={onKeydown} name="text-input" /> : false)}
         <div>
           {true && onClickAccept ? (
